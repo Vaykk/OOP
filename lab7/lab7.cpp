58e4e73b2bc28ff2a330b39148d170cb83d23e4f
Vaykk <130344620+Vaykk@users.noreply.github.com>
2023-04-26 23:21:44 +0500

    Add files via upload

---
+++ lab7/lab7.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Time {
+public:
+	Time(int minutes = 0, int seconds = 0) : minutes_(minutes), seconds_(seconds) {}
+	void setMinutes(int minutes) { minutes_ = minutes; }
+	void setSeconds(int seconds) { seconds_ = seconds; }
+	int getMinutes() const { return minutes_; }
+	int getSeconds() const { return seconds_; }
+private:
+	int minutes_;
+	int seconds_;
+};
+
+void inputVector(vector<int>& v) {
+	int n;
+	cout << "Enter the size of the vector: ";
+	cin >> n;
+	v.resize(n);
+	for (int i = 0; i < n; i++) {
+		cout << "Enter element " << i << ": ";
+		cin >> v[i];
+	}
+}
+
+void inputTime(Time& t) {
+	int m, s;
+	cout << "Enter minutes: ";
+	cin >> m;
+	cout << "Enter seconds: ";
+	cin >> s;
+	t = Time(m, s);
+}
+
+void printVector(const vector<int>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
+void printTime(const Time& t) {
+	cout << t.getMinutes() << ":" << t.getSeconds() << endl;
+}
+
+class VectorInt {
+public:
+	VectorInt() {}
+	VectorInt(int size) : elements_(size) {}
+	VectorInt(const VectorInt& other) : elements_(other.elements_) {}
+	~VectorInt() {}
+	void input() {
+		inputVector(elements_);
+	}
+
+	void print() const {
+		printVector(elements_);
+	}
+
+	int operator[](int index) const {
+		return elements_[index];
+	}
+
+	int& operator[](int index) {
+		return elements_[index];
+	}
+
+	VectorInt operator+(const VectorInt& other) const {
+		VectorInt result(elements_.size());
+		for (int i = 0; i < elements_.size(); i++) {
+			result[i] = elements_[i] + other[i];
+		}
+		return result;
+	}
+
+	int operator+() const {
+		return elements_.size();
+	}
+private:
+	vector<int> elements_;
+};
+
+int main() {
+	VectorInt v1, v2;
+	cout << "Enter the first vector:" << endl;
+	v1.input();
+	cout << "Enter the second vector:" << endl;
+	v2.input();
+	cout << "First vector elements: ";
+	v1.print();
+	cout << "Second vector elements: ";
+	v2.print();
+	VectorInt v3 = v1 + v2;
+	cout << "Sum of vectors: ";
+	v3.print();
+
+	int size = +v1;
+	cout << "Size of the first vector: " << size << endl;
+
+	Time t1, t2;
+	cout << "Enter the first time interval:" << endl;
+	inputTime(t1);
+	cout << "Enter the second time interval:" << endl;
+	inputTime(t2);
+	cout << "First time interval: ";
+	printTime(t1);
+	cout << "Second time interval: ";
+	printTime(t2);
+
+	return 0;
+}
---
+++ lab8/lab8.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Базовый класс
+class Person {
+public:
+	Person(string name, int age) : name(name), age(age) {}
+	virtual ~Person() {}
+	virtual void printInfo() const = 0;  // чисто виртуальный метод
+public:
+	string name;
+	int age;
+};
+// Производный класс
+class Employee : public Person {
+public:
+	Employee(string name, int age, float salary, string position)
+		: Person(name, age), salary(salary), position(position) {}
+	void printInfo() const override {
+		cout << "Employee: " << name << ", Age: " << age << ", Salary: " << salary << ", Position: " << position << endl;
+	}
+
+	float getSalary() const {
+		return salary;
+	}
+
+	string getPosition() const {
+		return position;
+	}
+private:
+	float salary;
+	string position;
+};
+
+// Класс-группа
+class Group {
+public:
+	Group() {}
+	~Group() {
+		for (auto& p : people) {
+			delete p;
+		}
+	}
+	void addPerson(Person* p) {
+		people.push_back(p);
+	}
+
+	void removePerson(int index) {
+		delete people[index];
+		people.erase(people.begin() + index);
+	}
+
+	void printInfo() const {
+		for (const auto& p : people) {
+			p->printInfo();
+		}
+	}
+
+	double averageAge() const {
+		double sum = 0;
+		for (const auto& p : people) {
+			sum += p->age;
+		}
+		return sum / people.size();
+	}
+
+	int size() const {
+		return people.size();
+	}
+
+	Person* getPerson(int index) {
+		return people[index];
+	}
+private:
+	vector<Person*> people;
+};
+
+int main() {
+	Group group;
+	while (true) {
+		char command;
+		cout << "Enter command (+ to add, - to remove, s to show, z to show by index, q to quit): ";
+		cin >> command;
+
+		if (command == '+') {
+			string name, position;
+			int age;
+			float salary;
+
+			cout << "Enter name: ";
+			cin >> name;
+
+			cout << "Enter age: ";
+			cin >> age;
+
+			cout << "Enter salary: ";
+			cin >> salary;
+
+			cout << "Enter position: ";
+			cin >> position;
+
+			group.addPerson(new Employee(name, age, salary, position));
+		}
+		else if (command == '-') {
+			int index;
+			cout << "Enter index to remove: ";
+			cin >> index;
+
+			if (index >= 0 && index < group.size()) {
+				group.removePerson(index);
+			}
+			else {
+				cout << "Index is out of range!" << endl;
+			}
+		}
+		else if (command == 's') {
+			group.printInfo();
+		}
+		else if (command == 'z') {
+			int index;
+			cout << "Enter index to show: ";
+			cin >> index;
+
+			if (index >= 0 && index < group.size()) {
+				group.getPerson(index)->printInfo();
+			}
+			else {
+				cout << "Index is out of range!" << endl;
+			}
+		}
+		else if (command == 'q') {
+			break;
+		}
+		else {
+			cout << "Unknown command!" << endl;
+		}
+	}
+
+	return 0;
+}
+
+
